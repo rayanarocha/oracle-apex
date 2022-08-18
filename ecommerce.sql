f8d2e11e4ad14eae983660e2e781e24a36e78c32
rayanarocha <rayana.azus@gmail.com>
2022-08-18 11:37:03 -0300

    Adicionandos algumas tabelas, inserts e consultas

--- ecommerce.sql
+++ ecommerce.sql
@@ -1,3 +1,5 @@
+
+-- Object type, tabela, drop, sequence, insert e select referente ao Endereço
 create TYPE endereco_type as object (
     rua varchar(20),
     cidade varchar(10),
@@ -7,14 +9,19 @@ create TYPE endereco_type as object (
 /
 
 create table endereco of endereco_type;
+drop table endereco;
 
 insert into endereco values (endereco_type('Rua Beco sem saída', 'Coxixola', 'PB', '58000000'));
-
 select * from endereco;
 
+-----------------------------------------------------------------------------------------------
+
+-- Cria coleção de telefone
 create type fone_lista_type as varray(10) of varchar(20);
 /
+------------------------------------------------------------------------------------------------
 
+-- Object type, tabela, drop, sequence, insert e select referente ao Cliente
 create type cliente_type as object (
     codigo number,
     nome varchar(80),
@@ -24,29 +31,71 @@ create type cliente_type as object (
 /
 
 create table cliente of cliente_type;
-insert into cliente values (1, 'Severino Biu', endereco_type('Rua Beco sem saída', 'Coxixola', 'PB', '58000000'), fone_lista_type('839912349876'));
+drop table cliente;
+
+CREATE SEQUENCE cliente_seq
+ START WITH     1
+ INCREMENT BY   1
+ NOCACHE
+ NOCYCLE;
+ 
+drop sequence cliente_seq;
+
+insert into cliente values (cliente_seq.nextval, 'Severino Biu', endereco_type('Rua Beco sem saída', 'Coxixola', 'PB', '58000000'), fone_lista_type('839912349876'));
+insert into cliente values (cliente_type(cliente_seq.nextval, 'Ana Maria', endereco_type('Rua Beco sem saída', 'Coxixola', 'PB', '58000000'), fone_lista_type('839912349876')));
 -- Consulta a tabela toda
 select * from cliente;
 -- Consulta cada atributo individualmente para mostrar os atributos que são dos tipos de objetos criados
 select c.codigo, c.nome, c.endereco.rua, c.endereco.cidade, c.endereco.estado, c.endereco.cep, c.listafone from cliente c;
+---------------------------------------------------------------------------------------------------------------------------------------------
 
+-- Object type, tabela, drop, insert e select referente ao Cliente Vip
 create type cliente_vip_type under cliente_type (
     pontos integer,
     desconto number
 );
 /
 
+create table cliente_vip of cliente_vip_type;
+drop table cliente_vip;
+
+insert into cliente_vip values (cliente_seq.nextval, 'João José', endereco_type('Rua Beco sem saída', 'Coxixola', 'PB', '58000000'), fone_lista_type('839912349876'), 10, 5);
+select * from cliente_vip;
+-----------------------------------------------------------------------------------------------------
+
+-- Object type, tabela, drop, insert e select referente ao Cliente Especial
 create type cliente_especial_type under cliente_type (
     desconto number
 );
 /
 
+create table cliente_especial of cliente_especial_type;
+drop table cliente_especial;
+
+insert into cliente_especial values (cliente_seq.nextval, 'Pedro Paulo', endereco_type('Rua Beco sem saída', 'Coxixola', 'PB', '58000000'), fone_lista_type('839912349876'), 10);
+select * from cliente_especial;
+-----------------------------------------------------------------------------------------------------
+
+-- Object type, tabela, drop, sequence, insert e select referente ao Produto
 create type produto_type as object (
     codigo number,
     preco number,
     taxa number
 );
 /
+
+create table produto of produto_type;
+drop table produto;
+
+CREATE SEQUENCE produto_seq
+ START WITH     1
+ INCREMENT BY   1
+ NOCACHE
+ NOCYCLE;
+ 
+insert into produto values (produto_type(produto_seq.nextval, 100, 10));
+select * from produto;
+-----------------------------------------------------------------------------------------------------
 
 create type item_type as object (
     codigo number,
