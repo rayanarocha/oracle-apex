da1f3be7041b5694023f555cb6b4c4e1fbf15bc8
rayanarocha <rayana.azus@gmail.com>
2022-08-18 10:07:44 -0300

    Adicionandos algumas tabelas, inserts e consultas

--- ecommerce.sql
+++ ecommerce.sql
@@ -6,6 +6,12 @@ create TYPE endereco_type as object (
 );
 /
 
+create table endereco of endereco_type;
+
+insert into endereco values (endereco_type('Rua Beco sem saída', 'Coxixola', 'PB', '58000000'));
+
+select * from endereco;
+
 create type fone_lista_type as varray(10) of varchar(20);
 /
 
@@ -16,6 +22,13 @@ create type cliente_type as object (
     listafone fone_lista_type
 )not final;
 /
+
+create table cliente of cliente_type;
+insert into cliente values (1, 'Severino Biu', endereco_type('Rua Beco sem saída', 'Coxixola', 'PB', '58000000'), fone_lista_type('839912349876'));
+-- Consulta a tabela toda
+select * from cliente;
+-- Consulta cada atributo individualmente para mostrar os atributos que são dos tipos de objetos criados
+select c.codigo, c.nome, c.endereco.rua, c.endereco.cidade, c.endereco.estado, c.endereco.cep, c.listafone from cliente c;
 
 create type cliente_vip_type under cliente_type (
     pontos integer,
