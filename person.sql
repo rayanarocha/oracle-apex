a8aeeb60859c792454805f0b4a13e46bc0a1dcb5
rayanarocha <rayana.azus@gmail.com>
2022-08-25 14:15:29 -0300

    object type t_person2, tabela object_customers e sobrecarga do construtor

--- person.sql
+++ person.sql
@@ -8,7 +8,47 @@ CREATE TYPE person_typ AS OBJECT (
     MEMBER PROCEDURE display_details(SELF IN OUT person_typ)
 ) not final;
     
+-- overloadable do construtor
+
+create type t_person2 as object (
+    id integer,
+    first_name varchar2(10),
+    last_name varchar2(10),
+    dob date,
+    phone varchar2(12),
+    constructor function t_person2(p_id integer, p_first_name varchar2, p_last_name varchar2) 
+    return self as result
+);
+
+create or replace type body t_person2 as
+constructor function t_person2 (
+    p_id integer,
+    p_first_name varchar2,
+    p_last_name varchar2
+) return self as result is
+begin
+    self.id := p_id;
+    self.first_name := p_first_name;
+    self.last_name := p_last_name;
+    self.dob := sysdate;
+    self.phone := '5551212';
+    return;
+end;
+end;
+
+create table object_customers2 of t_person2;
+
+insert into object_customers2 values (t_person2(person2_seq.nextval, 'Jeff', 'Jones'));
+
+insert into object_customers2 values (t_person2(person2_seq.nextval, 'Mary', 'Jones', '01-JAN-2012','8887-9876'));
+
 CREATE SEQUENCE person_seq
+ START WITH     1
+ INCREMENT BY   1
+ NOCACHE
+ NOCYCLE;
+ 
+CREATE SEQUENCE person2_seq
  START WITH     1
  INCREMENT BY   1
  NOCACHE
@@ -34,3 +74,19 @@ END;
 
 select p.get_idno()
 from person_tab p;
+
+create or replace type body t_person2 as
+constructor function t_person2 (
+    p_id integer,
+    p_first_name varchar2,
+    p_last_name varchar2
+) return self as result is
+begin
+    self.id := p_id;
+    self.first_name := p_first_name;
+    self.last_name := p_last_name;
+    self.dob := sysdate;
+    self.phone := '5551212';
+    return;
+end;
+end;
