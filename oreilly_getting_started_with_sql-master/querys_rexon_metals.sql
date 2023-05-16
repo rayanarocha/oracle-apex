85ed940717673f1165427f53fb44814dbbb45c67
Rayana Rocha <rayana.azus@gmail.com>
2023-05-16 16:37:21 -0300

    operacoes no banco rexon

--- oreilly_getting_started_with_sql-master/querys_rexon_metals.sql
+++ oreilly_getting_started_with_sql-master/querys_rexon_metals.sql
@@ -6,3 +6,27 @@ select customer_id, name from customer;
 
 -- selecionando todas as linhas da tabelas PRODUCT
 select * from product;
+
+-- calculando +7% acima de PRICE
+select 
+product_id, 
+description, 
+price, 
+price * 1.07 as taxed_price 
+from product;
+
+-- alterando o nome da coluna PRICE para UNTAXED_PRICE dentro da instrução SELECT
+select
+product_id,
+description,
+price as untaxed_price,
+price * 1.07 as taxed_price
+from product;
+
+-- formatando casas decimais da coluna TAXED_PRICE
+select
+product_id,
+description,
+price,
+round(price * 1.07, 2) as taxed_price
+from product;
