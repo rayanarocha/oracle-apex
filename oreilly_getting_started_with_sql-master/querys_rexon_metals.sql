21395857e3b6b18d51597f78595636a2ed6edf76
Rayana Rocha <rayana.azus@gmail.com>
2023-05-16 16:07:36 -0300

    operacoes no banco rexon

---
+++ oreilly_getting_started_with_sql-master/querys_rexon_metals.sql
@@ -0,0 +1,8 @@
+--consultando todas as as linhas da tabela CUSTOMER
+select * from customer;
+
+-- consultando apenas as tabelas CUSTOMER_ID e NAME da tabela CUSTOMER
+select customer_id, name from customer;
+
+-- selecionando todas as linhas da tabelas PRODUCT
+select * from product;
